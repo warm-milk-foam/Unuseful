f5f9065d79a3673f524d8c069d3a561d94da9e1f
srfvidov <fkor3@AMK.cpq>
2023-06-24 11:47:35 +0800

    Initial Commit

---
+++ Unuseful/UnusefulApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnusefulApp.swift
+//  Unuseful
+//
+//  Created by T Krobot on 24/6/23.
+//
+
+import SwiftUI
+
+@main
+struct UnusefulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UnusefulUITests/UnusefulUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  UnusefulUITestsLaunchTests.swift
+//  UnusefulUITests
+//
+//  Created by T Krobot on 24/6/23.
+//
+
+import XCTest
+
+final class UnusefulUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
