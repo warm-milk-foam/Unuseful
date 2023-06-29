b18ec0486f17abeb846642526f5b3e2c468b1f16
srfvidov <fkor3@AMK.cpq>
2023-06-29 14:20:43 +0800

    i need to eat so just this for now

--- Unuseful/ContentView.swift
+++ Unuseful/ContentView.swift
@@ -9,12 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var starMessages = ["Sir dont share your pee colour in this server", "I just wasted all my super reactions on this, what a great use of $99", "Anyway please continue being ridiculous","Ok nvm I'm cringe", "(Never)", "yuhan trans?????", "That forehead is wider than my sister"]
+    var pictures = [Image("froggy")]
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
+            Image("froggy") // test image
+                .resizable()
+                .scaledToFit()
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+                .font(.system(size: 40))
         }
         .padding()
     }
