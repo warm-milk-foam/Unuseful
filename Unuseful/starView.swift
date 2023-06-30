8fb3733716cfc2cc73573ae1fe947523b3c1c078
PhyoTP <phyo.thet.pai@sji.edu.sg>
2023-06-30 22:59:23 +0800

    added more star messages

--- Unuseful/starView.swift
+++ Unuseful/starView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 struct starView: View {
-    var starMessages = ["Sir dont share your pee colour in this server", "I just wasted all my super reactions on this, what a great use of $99", "Anyway please continue being ridiculous","Ok nvm I'm cringe", "(Never)", "yuhan trans?????", "That forehead is wider than my sister"]
+    var starMessages = ["Sir dont share your pee colour in this server", "I just wasted all my super reactions on this, what a great use of $99", "Anyway please continue being ridiculous","Ok nvm I'm cringe", "(Never)", "yuhan trans?????", "That forehead is wider than my sister","Swift Innovator's Summit 2024: Our grand plan to get rid of you","we do not condone hackintoshing","Sir that’s a war crime","IM DA BIGGEST BIRD IM DA BIGGEST BIRD","Its going to my media","Unless you're james or smth"]
     @State private var currentMessage = ""
     var body: some View {
         VStack{
