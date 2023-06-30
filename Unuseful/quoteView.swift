b2af0f047a57edfbf5fcd7d789b237fd8c3f5a9f
Ingo <ingocodingbecauseyes@gmail.com>
2023-06-30 16:36:10 +0800

    Added quotes from #quotes

--- Unuseful/quoteView.swift
+++ Unuseful/quoteView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+var quotes = ["\"when you fork you can create many different things\" -yj 2023", "\"Generative AIs are prone to hallucination\" -jy soon", "\"bad bot bad bot\" -yjsoon to saumil gpt", "\"i can almost hear the students’ collective moans” - @Kai (2022)\"", "\"Slowmode will never stop our endeavour to count in the wrong places\" -ingo wawa"]
 struct quoteView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
