441bbefd2dced5275ca7ebf5fee6eadcbc3f62e0
PhyoTP <phyo.thet.pai@sji.edu.sg>
2023-07-01 15:16:16 +0800

    added animations to saumil says

--- Unuseful/starView.swift
+++ Unuseful/starView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 struct starView: View {
     var starMessages = ["Sir dont share your pee colour in this server", "I just wasted all my super reactions on this, what a great use of $99", "Anyway please continue being ridiculous","Ok nvm I'm cringe", "(Never)", "yuhan trans?????", "That forehead is wider than my sister","Swift Innovator's Summit 2024: Our grand plan to get rid of you","we do not condone hackintoshing","Sir that’s a war crime","IM DA BIGGEST BIRD IM DA BIGGEST BIRD","Its going to my media","Unless you're james or smth","HOW DARE YOU","u chose to join the challenge that’s your problem","FRICK WE MISSED THE OPPORTUNITY TO ASK THEM WHEN WAS APPLE FOUNDED", "Why is yours so normal","i hate swift so much"]
     @State private var currentMessage = ""
-    @State private var yLevel = 0
+    @State private var yLevel1 = 0
+    @State private var yLevel2 = 0
     var body: some View {
         VStack{
             Image("saumil")
             Button{
                 currentMessage = starMessages.randomElement()!
-                
+                yLevel2 = -1000
+                yLevel1 = 10
+                withAnimation(){
+                    yLevel2 = 0
+                    yLevel1 = 0
+                }
             }label: {
                 ZStack{
                     Image(systemName: "button.programmable")
@@ -23,15 +29,16 @@ struct starView: View {
                         .rotation3DEffect(Angle(degrees: 60.0), axis: (x:60,y:0,z:0))
                     Text("Press here")
                         .foregroundColor(.black)
-                        .offset(y: CGFloat(yLevel))
                         .rotation3DEffect(Angle(degrees: 40.0), axis: (x:40,y:0,z:0))
                 }
+                .offset(y: CGFloat(yLevel1))
             }
             Text("Saumil says..")
                 .bold()
                 .font(.system(size: 30))
             Text(currentMessage)
                 .multilineTextAlignment(.center)
+                .offset(y: CGFloat(yLevel2))
         }
     }
 }
