ccc56284f3683fd89fe518fadd875698323f26b2
Ingo <ingocodingbecauseyes@gmail.com>
2023-07-01 09:39:16 +0800

    idk

--- Unuseful/quoteView.swift
+++ Unuseful/quoteView.swift
@@ -6,12 +6,20 @@
 //
 
 import SwiftUI
-
+var bobo = 0
 var quotes = ["\"when you fork you can create many different things\" -yj 2023", "\"Generative AIs are prone to hallucination\" -jy soon", "\"bad bot bad bot\" -yjsoon to saumil gpt", "\"i can almost hear the students’ collective moans” - @Kai (2022)\"", "\"Slowmode will never stop our endeavour to count in the wrong places\" -ingo wawa"]
 struct quoteView: View {
     var body: some View {
-        Text(quotes[0])
-            .multilineTextAlignment(.center)
+        VStack{
+            Button{
+               print("tapped")
+            } label: {
+                Image(systemName: "trash.square.fill")
+                    .font(.system(size: 150))
+                
+            }
+            Text("Tap the pic")
+        }
     }
 }
 
