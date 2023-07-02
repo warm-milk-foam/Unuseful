4c01da415ae61fc6fe1fa0493709d7efab49391e
PhyoTP <phyo.thet.pai@sji.edu.sg>
2023-07-02 10:32:09 +0800

    changed background

--- Unuseful/creditView.swift
+++ Unuseful/creditView.swift
@@ -9,55 +9,61 @@ import SwiftUI
 
 struct creditView: View {
     var body: some View {
-        VStack{
-            
-            Text("Credits:💫")
-                .bold()
-                .font(.largeTitle)
-            Group{
-                Text("Photos Tab: Hongyu")
-                    .font(.title2)
-                Text("Photos from: The SAP community")
-                    .offset(x: 0,y:0)
-                 //Hongyu you  can add credits here
-            }
-            Group{
-                Text("Saumil says: Thet Pai")
-                    .font(.title2)
-                    .offset(y:0)
-                Text("Messages from: #bestof")
-                    .offset(x: 0,y:0)
-            }
-
-            Group{
-                Text("Home tab: Thet Pai")
-                    .font(.title2)
-                    .offset(x: 0,y:0)
-                Text("Canon event template: The projecttian on YouTube")
-                    .offset(x: 0,y:0)
-                Text("Meme made by: Thet Pai")
-                    .offset(x: 0,y:0)
-            }
-            Group{
-                Text("Quotes tab: 🅱️🅾️ngo")
-                    .font(.title2)
-                    .offset(x: 0,y:0)
-                Text("My epic meme storage has served me well")//Ingo you can add credits here
-                    .offset(x: 0,y:0)
+        ZStack{
+            Text("")
+                .padding(400)
+                .background(.linearGradient(colors: [.orange,.yellow], startPoint: .topLeading, endPoint: .bottomTrailing))
                 
+            VStack{
+                
+                Text("Credits:💫")
+                    .bold()
+                    .font(.largeTitle)
+                Group{
+                    Text("Photos Tab: Hongyu")
+                        .font(.title2)
+                    Text("Photos from: The SAP community")
+                        .offset(x: 0,y:0)
+                    //Hongyu you  can add credits here
+                }
+                Group{
+                    Text("Saumil says: Thet Pai")
+                        .font(.title2)
+                        .offset(y:0)
+                    Text("Messages from: #bestof")
+                        .offset(x: 0,y:0)
+                }
+                
+                Group{
+                    Text("Home tab: Thet Pai")
+                        .font(.title2)
+                        .offset(x: 0,y:0)
+                    Text("Canon event template: The projecttian on YouTube")
+                        .offset(x: 0,y:0)
+                    Text("Meme made by: Thet Pai")
+                        .offset(x: 0,y:0)
+                }
+                Group{
+                    Text("Quotes tab: 🅱️🅾️ngo")
+                        .font(.title2)
+                        .offset(x: 0,y:0)
+                    Text("My epic meme storage has served me well")//Ingo you can add credits here
+                        .offset(x: 0,y:0)
+                    
+                }
+                Text("Credits tab: Thet Pai and Ingo lol")
+                    .font(.title2)
+                    .offset(x: 0,y:0)
+                Image("5")
+                    .resizable()
+                    .frame(width: 250, height: 250)
+                    .offset(x: 0,y:0)
+                    .cornerRadius(500)
+                Text("thwank you OwO :3🥰💖")
+                    .offset(x: 0, y: 0)
+                    .foregroundColor(.blue)
+                // ingo didnt make this trust
             }
-            Text("Credits tab: Thet Pai and Ingo lol")
-                .font(.title2)
-                .offset(x: 0,y:0)
-            Image("5")
-                .resizable()
-                .frame(width: 250, height: 250)
-                .offset(x: 0,y:0)
-                .cornerRadius(500)
-            Text("thwank you OwO :3🥰💖")
-                .offset(x: 0, y: 0)
-                .foregroundColor(.blue)
-            // ingo didnt make this trust
         }
     }
 }
